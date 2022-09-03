65180a0a3b8216dd28b28ee43460d0fe53d4601a
Jaben Cargman <jaben@changemakerstudios.us>
2022-09-02 23:37:12 -0400

    Need a backwards compatible option for Contains() with StringComparison.

---
+++ src/Serilog.Sinks.MongoDB/Helpers/CollectionHelpers.cs
@@ -0,0 +1,16 @@
+﻿
+
+// ReSharper disable once CheckNamespace
+namespace System;
+#if !NETSTANDARD2_1
+internal static class CollectionHelpers
+{
+    internal static bool Contains(
+        this string? str,
+        string value,
+        StringComparison comparison)
+    {
+        return str?.IndexOf(value, comparison) >= 0;
+    }
+}
+#endif
